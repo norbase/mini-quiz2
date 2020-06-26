27c23c4b10b3f717fc1fc0de45374f0362e915d2
Neil Martin Orbase <orbase@Neils-MBP.attlocal.net>
2020-06-25 21:57:17 -0700

    updated comments and formatting

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
-// 1. Created a multidimensional array to hold quiz questions and answers
+// 1. Multidimensional array that holds quiz questions and answers
 const questions = [
-  [ "Who was the lead singer (full name) of Linkin Park?", "chester bennington" ],
-  [ "Which Linkin Park song was featured in the first live-action Transformers movie?", "what i've done" ],
-  [ "What is the last name of Linkin Park's 'rapper/lyricist'?", "shinoda" ],
-  [ "What was the name of the first Linkin Park 'remix' album?", "reanimation" ],
-  [ "Prior to July 2017, how many total members were in Linkin Park?", 6 ],
-  [ "What is the name of the Linkin Park/Jay-Z collaboration album?", "collision course"],
-  [ "Who was riding along with the Linkin Park on the show 'Carpool Karaoke'?", "ken jeong"],
-  [ "What is the title of Linkin Park's first debut single?", "one step closer"],
-  [ "Complete the missing lyrics: '...these wounds, they will not heal. _____________. Confusing what is real.'", "fear is how i fall"]
-  ];
-  
-  // 2. Store the number of questions answered correctly/incorrectly
-  const correct = [];
-  const incorrect = [];
-  
-  /* 
-    3. Using a loop to cycle through each question
-        - Present each question to the user
-        - Compare the user's response to answer in the array
-        - If the response matches the answer, the number of correctly
-          answered questions increments by 1
-  */
+  ["Who was the lead singer (full name) of Linkin Park?", "chester bennington"],
+  [
+    "Which Linkin Park song was featured in the first live-action Transformers movie?",
+    "what i've done",
+  ],
+  ["What is the last name of Linkin Park's 'rapper/lyricist'?", "shinoda"],
+  ["What was the name of the first Linkin Park 'remix' album?", "reanimation"],
+  ["Prior to July 2017, how many total members were in Linkin Park?", 6],
+  [
+    "What is the name of the Linkin Park/Jay-Z collaboration album?",
+    "collision course",
+  ],
+  [
+    "Who was riding along with the Linkin Park on the show 'Carpool Karaoke'?",
+    "ken jeong",
+  ],
+  ["What is the title of Linkin Park's first debut single?", "one step closer"],
+  [
+    "Complete the missing lyrics: '...these wounds, they will not heal. _____________. Confusing what is real.'",
+    "fear is how i fall",
+  ],
+];
 
+// 2. Containers that store the questions answered correctly/incorrectly
+const correct = [];
+const incorrect = [];
+
+
+/* 
+  3. If user reloads the page
+      - Alert is presented to user
+      - User is redirected to index page
+          Otherwise, script will continue to run
+*/
 function refreshPage() {
   if (performance.navigation.type === 1) {
     alert("Retry?! I mean... come on. In the end, it doesn't even matter. =P");
     // future version to include refresh to 'index.html' without script from 'page1' reloading
-    window.location.replace('index.html');
+    window.location.replace("index.html");
   } else {
     askAndCollect();
   }
 }
 refreshPage();
 
+/* 
+  4. Used a loop to cycle through each question
+      - Presents each question to the user
+      - Compares the user's response to answer in the array
+      - If the response matches the answer, correctly
+        answered questions are stored in the 'correct' container;
+          Otherwise, the incorrectly answered questions are stored
+          in the 'incorrect' container
+*/
 function askAndCollect(arr) {
-  // let correctCount = 0;
-
   for (let i = 0; i < questions.length; i++) {
     let question = questions[i][0];
     let answer = questions[i][1];
     let response = prompt(question);
-    
-    if (response === null || response === undefined || response === '') {
-      console.log("ERROR:", "User skipped this question: " + question + " Refresh page to try again.")
+
+    if (response === null || response === undefined || response === "") {
+      console.log(
+        "ERROR:",
+        "User skipped this question: " +
+          question +
+          " Refresh page to try again."
+      );
       if (response !== answer) {
-        incorrect.push(question); 
+        incorrect.push(question);
       }
-      continue; 
+      continue;
     }
     if (typeof answer === typeof 1) {
       response = parseInt(response);
@@ -71,25 +94,26 @@ function askAndCollect(arr) {
     } else if (response.trimRight() === "what i’ve done") {
       correct.push(question);
     } else {
-      incorrect.push(question);   
+      incorrect.push(question);
     }
   }
 }
 
-
+// 5. Returns total number of elements from 'correct' container
 function getTotals(arr) {
   return correct.length;
 }
 
-  function createListItems(arr) {
-    let items = '';
-    for (let i = 0; i < arr.length; i++) {
-      items += `<li>${arr[i]}</li>`;
-    }
-    return items;
+// 6. Creates list of items to be displayed onto page
+function createListItems(arr) {
+  let items = "";
+  for (let i = 0; i < arr.length; i++) {
+    items += `<li>${arr[i]}</li>`;
   }
-  
-  // 4. Displays the number of correct answers to the user
+  return items;
+}
+
+// 7. Sets up items to be displayed onto page
 function createHTML() {
   let html = `
   <h1>You got ${getTotals()} question(s) correct!</h1>
@@ -103,5 +127,7 @@ function createHTML() {
   return html;
 }
 
-const printStatus = () => document.querySelector('main').innerHTML = createHTML();
+// 8. Displays items onto second page
+const printStatus = () =>
+  (document.querySelector("main").innerHTML = createHTML());
 printStatus();
